8fc2015be4b4f3f8828fca0a07ff775c86c3b783
VanshikaSK <vsinghkalakoti05@gmail.com>
2026-08-14 21:12:32 +0530

    Update transaction type options in FraudDetectApp
    Removed 'DEPOSIT' option from transaction type selection.

--- FraudDetectApp.py
+++ FraudDetectApp.py
@@ -10,7 +10,7 @@ st.markdown("This application allows you to input transaction details and predic
 
 st.divider()
 
-transaction_type=st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT","DEPOSIT"])
+transaction_type=st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT"])
 amount=st.number_input("Amount", min_value=0.0, value=1000.0)
 oldbalanceOrg=st.number_input("Old Balance of Origin Account", min_value=0.0, value=10000.0)
 newbalanceOrig=st.number_input("New Balance of Origin Account", min_value=0.0, value=9000.0)
